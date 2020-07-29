93335b4e3acb7377ccbd6eebf6ca3dbc870b9f6b
Lady-Binary <ladybinary@protonmail.com>
2020-07-29 03:13:36 +0000

    adding gren file for release notes

---
+++ .grenrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    "dataSource": "issues",
+    "prefix": "LoA " + process.env.LOA_VERSION + " - EasyLOU ",
+    "onlyMilestones": false,
+    "groupBy": "label",
+    "changelogFilename": "CHANGELOG.md"
+}
