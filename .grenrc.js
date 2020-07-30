991c794f9421133ccc7c854ba03ed201dc12b576
Lady-Binary <ladybinary@protonmail.com>
2020-07-30 09:26:52 +0000

    add template for gren changelog

--- .grenrc.js
+++ .grenrc.js
@@ -3,5 +3,25 @@ module.exports = {
     "prefix": "LoA " + process.env.LOA_VERSION + " - EasyLOU ",
     "onlyMilestones": false,
     "groupBy": "label",
-    "changelogFilename": "CHANGELOG.md"
+    "changelogFilename": "CHANGELOG.md",
+	"template": {
+		issue: function (placeholders) {
+			return '- ' + placeholders.name + ' [' + placeholders.text + '](' + placeholders.url + ')';
+        },
+		noLabel: function (placeholders) {
+			return '';
+        },
+		group: function (placeholders) {
+			var heading = placeholders.heading;
+			switch (heading) {
+				case "enhancement":
+					heading = "Enhancements:";
+					break;
+				case "bug":
+					heading = "Bugfixes:";
+					break;
+			}
+            return '\n#### ' + heading + '\n';
+        }
+	}
 }
