df70c3bba38a01b553f3ef4497e65ede8c4ae99b
Lady-Binary <ladybinary@protonmail.com>
2020-08-19 09:00:07 +0000

    preparing clientstatus for strong typed variables
    related to #91

--- LOU/ClientStatus.cs
+++ LOU/ClientStatus.cs
@@ -9,31 +9,249 @@ namespace LOU
     {
         [ProtoMember(1)]
         public long TimeStamp;
+
+        [ProtoContract]
+        public struct CharacterInfoStruct
+        {
+            [ProtoMember(1)]
+            public string BACKPACKID;
+            [ProtoMember(2)]
+            public string CHARDIR;
+            [ProtoMember(3)]
+            public string CHARGHOST;
+            [ProtoMember(4)]
+            public string CHARID;
+            [ProtoMember(5)]
+            public string CHARNAME;
+            [ProtoMember(6)]
+            public string CHARPOSX;
+            [ProtoMember(7)]
+            public string CHARPOSY;
+            [ProtoMember(8)]
+            public string CHARPOSZ;
+            [ProtoMember(9)]
+            public string CHARSTATUS;
+            [ProtoMember(10)]
+            public string CHARWEIGHT;
+            [ProtoMember(11)]
+            public string CHESTID;
+            [ProtoMember(12)]
+            public string CHESTNAME;
+            [ProtoMember(13)]
+            public string HEADID;
+            [ProtoMember(14)]
+            public string HEADNAME;
+            [ProtoMember(15)]
+            public string LEFTHANDID;
+            [ProtoMember(16)]
+            public string LEFTHANDNAME;
+            [ProtoMember(17)]
+            public string LEGSID;
+            [ProtoMember(18)]
+            public string LEGSNAME;
+            [ProtoMember(19)]
+            public string RIGHTHANDID;
+            [ProtoMember(20)]
+            public string RIGHTHANDNAME;
+        }
         [ProtoMember(2)]
-        public Dictionary<String, String> CharacterInfo = new Dictionary<string, String>();
+        public CharacterInfoStruct CharacterInfo;
+
+        [ProtoContract]
+        public struct StatusBarStruct
+        {
+            [ProtoMember(1)]
+            public string AGI;
+            [ProtoMember(2)]
+            public string ATTACKSPEED;
+            [ProtoMember(3)]
+            public string DEFENSE;
+            [ProtoMember(4)]
+            public string HEALTH;
+            [ProtoMember(5)]
+            public string INT;
+            [ProtoMember(6)]
+            public string MANA;
+            [ProtoMember(7)]
+            public string PRESTIGEXPMAX;
+            [ProtoMember(8)]
+            public string STAMINA;
+            [ProtoMember(9)]
+            public string STEALTH;
+            [ProtoMember(10)]
+            public string STR;
+            [ProtoMember(11)]
+            public string VITALITY;
+        }
         [ProtoMember(3)]
-        public Dictionary<String, String> StatusBar = new Dictionary<string, String>();
+        public StatusBarStruct StatusBar;
+
+        [ProtoContract]
+        public struct LastActionStruct
+        {
+            [ProtoMember(1)]
+            public string COBJECTID;
+            [ProtoMember(2)]
+            public string LOBJECTID;
+        }
         [ProtoMember(4)]
-        public Dictionary<String, String> ContainerInfo = new Dictionary<string, String>();
+        public LastActionStruct LastAction;
+
+        [ProtoContract]
+        public struct FindStruct
+        {
+            [ProtoMember(1)]
+            public string FINDBUTTONNAME;
+            [ProtoMember(2)]
+            public string FINDBUTTONTEXT;
+            [ProtoMember(3)]
+            public string FINDGAMEOBJECTID;
+            [ProtoMember(4)]
+            public string FINDITEMCNTID;
+            [ProtoMember(5)]
+            public string FINDITEMID;
+            [ProtoMember(6)]
+            public string FINDITEMNAME;
+            [ProtoMember(7)]
+            public string FINDLABELNAME;
+            [ProtoMember(8)]
+            public string FINDLABELTEXT;
+            [ProtoMember(9)]
+            public string FINDMOBILEDIST;
+            [ProtoMember(10)]
+            public string FINDMOBILEHP;
+            [ProtoMember(11)]
+            public string FINDMOBILEID;
+            [ProtoMember(12)]
+            public string FINDMOBILENAME;
+            [ProtoMember(13)]
+            public string FINDMOBILETYPE;
+            [ProtoMember(14)]
+            public string FINDPANELID;
+            [ProtoMember(15)]
+            public string FINDPERMACOLOR;
+            [ProtoMember(16)]
+            public string FINDPERMADIST;
+            [ProtoMember(17)]
+            public string FINDPERMAID;
+            [ProtoMember(18)]
+            public string FINDPERMANAME;
+            [ProtoMember(19)]
+            public string FINDPERMATEXTURE;
+            [ProtoMember(20)]
+            public string MONSTERSDIST;
+            [ProtoMember(21)]
+            public string MONSTERSHP;
+            [ProtoMember(22)]
+            public string MONSTERSID;
+            [ProtoMember(23)]
+            public string MONSTERSNAME;
+        }
         [ProtoMember(5)]
-        public Dictionary<String, String> LastAction = new Dictionary<string, String>();
+        public FindStruct Find;
+
+        [ProtoContract]
+        public struct ClientInfoStruct
+        {
+            [ProtoMember(1)]
+            public string CLIGAMESTATE;
+            [ProtoMember(2)]
+            public string CLIID;
+            [ProtoMember(3)]
+            public string CLIVER;
+            [ProtoMember(4)]
+            public string CLIXRES;
+            [ProtoMember(5)]
+            public string CLIYRES;
+            [ProtoMember(6)]
+            public string FULLSCREEN;
+            [ProtoMember(7)]
+            public string MAINCAMERAMASK;
+            [ProtoMember(8)]
+            public string SERVER;
+            [ProtoMember(9)]
+            public string TARGETFRAMERATE;
+            [ProtoMember(10)]
+            public string VSYNCCOUNT;
+        }
         [ProtoMember(6)]
-        public Dictionary<String, String> Find = new Dictionary<string, String>();
+        public ClientInfoStruct ClientInfo;
+
+        [ProtoContract]
+        public struct MiscellaneousStruct
+        {
+            [ProtoMember(1)]
+            public string CLICKDISPLAYNAME;
+            [ProtoMember(2)]
+            public string CLICKOBJCNTID;
+            [ProtoMember(3)]
+            public string CLICKOBJID;
+            [ProtoMember(4)]
+            public string CLICKOBJNAME;
+            [ProtoMember(5)]
+            public string CLICKPERID;
+            [ProtoMember(6)]
+            public string CLICKWINDOWX;
+            [ProtoMember(7)]
+            public string CLICKWINDOWY;
+            [ProtoMember(8)]
+            public string CLICKWINDOWZ;
+            [ProtoMember(9)]
+            public string CLICKWORLDX;
+            [ProtoMember(10)]
+            public string CLICKWORLDY;
+            [ProtoMember(11)]
+            public string CLICKWORLDZ;
+            [ProtoMember(12)]
+            public string COMMANDID;
+            [ProtoMember(13)]
+            public string MONSTERSNEARBY;
+            [ProtoMember(14)]
+            public string MOUSEOVERCNTID;
+            [ProtoMember(15)]
+            public string MOUSEOVERDISPLAYNAME;
+            [ProtoMember(16)]
+            public string MOUSEOVEROBJCNTID;
+            [ProtoMember(17)]
+            public string MOUSEOVEROBJID;
+            [ProtoMember(18)]
+            public string MOUSEOVEROBJNAME;
+            [ProtoMember(19)]
+            public string MOUSEOVERPERID;
+            [ProtoMember(20)]
+            public string MOUSEOVERUINAME;
+            [ProtoMember(21)]
+            public string MOUSEOVERUIX;
+            [ProtoMember(22)]
+            public string MOUSEOVERUIY;
+            [ProtoMember(23)]
+            public string MOUSEWINDOWX;
+            [ProtoMember(24)]
+            public string MOUSEWINDOWY;
+            [ProtoMember(25)]
+            public string MOUSEWINDOWZ;
+            [ProtoMember(26)]
+            public string MOUSEWORLDX;
+            [ProtoMember(27)]
+            public string MOUSEWORLDY;
+            [ProtoMember(28)]
+            public string MOUSEWORLDZ;
+            [ProtoMember(29)]
+            public string RANDOM;
+            [ProtoMember(30)]
+            public string SCANJOURNALMESSAGE;
+            [ProtoMember(31)]
+            public string SCANJOURNALTIME;
+            [ProtoMember(32)]
+            public string TARGETLOADING;
+            [ProtoMember(33)]
+            public string TARGETTYPE;
+            [ProtoMember(34)]
+            public string TIME;
+            [ProtoMember(35)]
+            public string TOOLTIPTEXT;
+        }
         [ProtoMember(7)]
-        public Dictionary<String, String> ShopInfo = new Dictionary<string, String>();
-        [ProtoMember(8)]
-        public Dictionary<String, String> ExtendedInfo = new Dictionary<string, String>();
-        [ProtoMember(9)]
-        public Dictionary<String, String> ClientInfo = new Dictionary<string, String>();
-        [ProtoMember(10)]
-        public Dictionary<String, String> CombatInfo = new Dictionary<string, String>();
-        [ProtoMember(11)]
-        public Dictionary<String, String> TileInfo = new Dictionary<string, String>();
-        [ProtoMember(12)]
-        public Dictionary<String, String> TimeInfo = new Dictionary<string, String>();
-        [ProtoMember(13)]
-        public Dictionary<String, String> Miscellaneous = new Dictionary<string, String>();
-        [ProtoMember(14)]
-        public Dictionary<String, String> ResultVariables = new Dictionary<string, String>();
+        public MiscellaneousStruct Miscellaneous;
     }
 }
