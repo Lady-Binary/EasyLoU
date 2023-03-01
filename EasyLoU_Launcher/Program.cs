02bd60bb204e6f6d82d36387ff6f602968bcc7cc
Lady-Binary <ladybinary@protonmail.com>
2023-03-01 14:21:50 +0000

    check sob launcher hash to prevent putting users at risk

--- EasyLoU_Launcher/Program.cs
+++ EasyLoU_Launcher/Program.cs
@@ -2,6 +2,7 @@ using System;
 using System.Diagnostics;
 using System.IO;
 using System.Reflection;
+using System.Security.Cryptography;
 
 class Program
 {
@@ -33,6 +34,44 @@ class Program
         var version = Assembly.GetExecutingAssembly().GetName().Version.ToString();
         Console.WriteLine();
         Console.WriteLine($"EasyLoU_Launcher v{version} - made by Lady Binary with \u2665");
+        Console.WriteLine();
+
+        Console.WriteLine("Checking if SoB_Launcher is running...");
+        var sobLauncherProcess = Process.GetProcessesByName("SoB_Launcher");
+        if (sobLauncherProcess == null || sobLauncherProcess.Length == 0)
+        {
+            Console.WriteLine();
+            Console.WriteLine("ERROR: SoB_Launcher not found.");
+            Console.WriteLine("Please start the SoB_Launcher first, then launch the game client, and then start EasyLoU_Launcher.");
+            Console.WriteLine();
+            Console.WriteLine("Please hit Enter to terminate.");
+            Console.ReadLine();
+            Environment.Exit(-1);
+        }
+
+        string sobLauncherFileName = sobLauncherProcess[0].MainModule.FileName;
+        string KNOWN_SOBLAUNCHER_HASH = "08ba8c5c4d2bfc5c0558774d850a45f102667eb24f1f58cc41805017dcc98dae";
+        BufferedStream inputStream = new BufferedStream(File.OpenRead(sobLauncherFileName), 1200000);
+        string sobLauncherHash = BitConverter.ToString(new SHA256Managed().ComputeHash(inputStream)).Replace("-", string.Empty).ToLowerInvariant();
+        if (sobLauncherHash != KNOWN_SOBLAUNCHER_HASH)
+        {
+            Console.WriteLine("WARNING: it looks like SoB_Launcher was updated!");
+            Console.WriteLine("This means it may contain new anticheat features that could put you in danger.");
+            Console.WriteLine("Please ask for guidance on the EasyLoU Discord before proceeding.");
+            Console.WriteLine();
+            Console.WriteLine("Press 'y' if you know what you are doing and would like to continue at your own risk.");
+            ConsoleKeyInfo key = Console.ReadKey();
+            if (key.Key.ToString() != "y" && key.Key.ToString() != "Y")
+            {
+                Environment.Exit(-1);
+            }
+            Console.WriteLine();
+        }
+        else
+        {
+            Console.WriteLine("...SoB_Launcher version is known, all good, we can continue.");
+        }
+
         Console.WriteLine();
 
         Console.WriteLine("Staging EasyLoU into temporary folder...");
